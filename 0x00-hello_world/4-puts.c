8679e41a2382b50cbe9fb52505019d32e107acdc
Oasis808 <isaiahadiamah20@gmail.com>
2023-08-10 20:39:53 -0700

    using printf to create a new world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - printing a line of code with puts function
- * return 0; (succes)
- **/
+ * return: 0 (succes)
+**/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0); (suceeded)
+return (0); (success)
 }
