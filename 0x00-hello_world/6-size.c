a014eba1a7e483d7a6839cf0f224aef16e48ed09
Oasis808 <isaiahadiamah20@gmail.com>
2023-08-10 22:13:08 -0700

    c in motion

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- * main - a code that prints the various return types
- * return:Always 0 (success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 printf("Size of a char: %d byte(s)\n", sizeof(char));
