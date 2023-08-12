12c75d1cd8183a678374428a587027992dd66f59
Oasis808 <isaiahadiamah20@gmail.com>
2023-08-12 11:18:47 +0000

    C in motion

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,11 +3,12 @@
  * main - prints all single digit numbers of base 10 starting from 0
  *
  * Return: Always 0 (Success)
- * 
+ *
  */
 int main(void)
 {
 	int n;
+
 	for (n = 48; n < 58; n++)
 	{
 	putchar(n);
