04ce04050780d120447db8684326b8bfa67237fd
Oasis808 <isaiahadiamah20@gmail.com>
2023-08-12 00:11:24 +0000

    Positive anything is better than negative nothing

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int n;
+int last_digit;
+srand(time(0));
+n= rand() - RAND_MAX /2;
+lastnum = n % 10;
+if (last_digit > 5)
+{
+
+}
+}
