f337f47f2b7c4b45d8608b987a2e24d582950c96
Oasis808 <isaiahadiamah20@gmail.com>
2023-08-12 11:10:15 +0000

    C in motion

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,11 +3,12 @@
  * main - prints the alphabets
  *
  * Return: Always 0 (Success)
- * 
+ *
  */
 int main(void)
 {
 	int ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	putchar(ch);
 	for (ch = 'A'; ch <= 'Z'; ch++)
