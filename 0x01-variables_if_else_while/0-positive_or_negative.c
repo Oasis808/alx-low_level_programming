9ddc91eac5565e822b80ef316595cde12792b87d
Oasis808 <isaiahadiamah20@gmail.com>
2023-08-12 00:34:20 +0000

    The last digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ *
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	int n;
