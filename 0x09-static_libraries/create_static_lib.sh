419c55b779cba41e779f88de98929fe5b67e9566
oasis808 <isaiahadiamah20@gmail.com>
2023-10-03 18:38:25 +0000

    Script on static library

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc  -Wall -pendantic -Werror -Wextra -c *.c
-ar rc liball.a *.o
-ranlib liball.a
+gcc  -Wall -Werror -Wextra -pedantic -std=gnu89 *.c
+ar rc libmy.a *.o
+ranlib libmy.a
