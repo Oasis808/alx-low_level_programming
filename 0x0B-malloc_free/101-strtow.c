d20d5bc1f30935c159d36cc6096f5f2cd439a0be
oasis808 <isaiahadiamah20@gmail.com>
2023-10-04 06:48:25 +0000

    I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -14,7 +14,7 @@ flag = 0;
 w = 0;
 for (c = 0; s[c] != '\0'; c++)
 {
-if (s[c] == ' ';
+if (s[c] == ' ')
 flag = 0;
 else if (flag == 0)
 {
@@ -46,7 +46,7 @@ if (matrix == NULL)
 return (NULL);
 for (i = 0; i <= len; i++)
 {
-if (str[i] == ' ' || str[i] == '\0';
+if (str[i] == ' ' || str[i] == '\0')
 {
 if (c)
 {
