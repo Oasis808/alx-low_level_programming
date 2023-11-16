5bf9ac66173fe1c289ddb3b80a35b95342dc20a7
oasis808 <isaiahadiamah20@gmail.com>
2023-11-16 17:48:22 +0000

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,18 @@
 #include "main.h"
 /**
- * _puts_recursion - function like puts();
- * @s: input
- * This function prints a string recursively.
- * Parameters:
- * s - Pointer to the string to be printed.
+ * _putchar - Prints a single character.
+ * @c: The character to be printed
+ *
  * Return: Always 0 (Success)
  */
 void _putchar(char c);
+
+/**
+ * _puts_recursion - Prints a string followed by a newline.
+ * @s: The input string
+ *
+ * Return: Always 0 (Success)
+ */
 void _puts_recursion(char *s)
 {
 if (*s)
@@ -16,5 +21,7 @@ _putchar(*s);
 _puts_recursion(s + 1);
 }
 else
+{
 _putchar('\n');
+}
 }
